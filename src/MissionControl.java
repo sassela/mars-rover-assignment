e094bc7f3c1bc22d8b05475f1ad048fa2a4a7378
= <sassela@live.com>
2015-08-10 14:28:09 +0100

    Add deployRover method

---
+++ src/MissionControl.java
@@ -0,0 +1,30 @@
+public class MissionControl {
+
+	Plateau plateau;
+	String[] headingsOptions = new String[] {"N", "E", "S", "W"};
+
+	public void deployRover(Plateau plateau, String position){
+		Rover rover = createRoverAtPosition(position);
+		if(!plateau.isRoverAt(position)) {
+			plateau.addRover(rover);
+		}
+	}
+
+	public Rover createRoverAtPosition(String position) {
+		String[] positionArray = position.split(" ");
+		int[] coordinates = new int[]{Integer.parseInt(positionArray[0]),Integer.parseInt(positionArray[1])};
+		int heading = parseHeading(positionArray[2]);
+		Rover rover = new Rover(plateau, coordinates, heading);
+		return(rover);
+	}
+
+	//todo delete
+	public int parseHeading(String currentHeading) {
+		int headingInt = 0;
+		for(int i = 0; i < currentHeading.length(); i++){
+			if (headingsOptions[i].equals(currentHeading)) headingInt = i;
+		}
+		return headingInt;
+	}
+
+}
