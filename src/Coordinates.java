3854a17ac14fb791e0cff86566db5c5a5fba75b5
= <sassela@live.com>
2015-08-17 16:00:35 +0100

    Refactor: Add parseCoordinates

--- src/Coordinates.java
+++ src/Coordinates.java
@@ -8,6 +8,14 @@ public class Coordinates {
 		this.y = y;
 	}
 
+	Coordinates parseCoordinates(String str) {
+		// TODO catch exception.
+		String[] positionArray = str.split(" ");
+		x = Integer.parseInt(positionArray[0]);
+		y = Integer.parseInt(positionArray[1]);
+		return new Coordinates(x, y);
+	}
+
 	@Override
 	public String toString() {
 		return x + " " + y;
