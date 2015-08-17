ef57b126b8536b5b355c63a5045b8bbdf1a73c46
= <sassela@live.com>
2015-08-17 10:40:02 +0100

    Add testNormalise

--- test/HeadingTest.java
+++ test/HeadingTest.java
@@ -2,14 +2,12 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class HeadingTest {
-	@Test
-	public void testValue() {
-		assertEquals(0, Heading.N.getValue());
-	}
 
 	@Test
-	public void testName() {
-		assertEquals("N", Heading.N.name());
+	public void testNormalise() {
+		assertEquals(0, Heading.normalise(4));
+		assertEquals(2, Heading.normalise(2));
+		assertEquals(3, Heading.normalise(-1));
 	}
 
 }
